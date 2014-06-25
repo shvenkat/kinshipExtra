c35a3965fa24e5b61c2ef83976fc6bb91083eaad
Shiv Venkatasubrahmanyam <shvenkat@amgen.com>
2014-06-25 09:19:41 -0700

    Tweaked build to work around Cygwin charset issues

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 echo "BUILDING PACKAGE DOCUMENTATION ..."
 R --slave -e 'library(roxygen2); roxygenize(clean = TRUE)'
+if [[ $(uname -s) =~ "CYGWIN_" ]]; then
+    sed -e 's/\xc2\xa0/ /g' -i man/*.Rd
+fi
 echo "BUILDING PACKAGE TARBALL in parent directory"
 R --slave -e 'library(devtools); build()'
-echo "DONE. Package can be installed with R CMD install"
+echo "DONE. Package can be installed with R CMD INSTALL"
